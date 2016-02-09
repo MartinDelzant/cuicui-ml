1bbf02e8d29b7df5afbbab409cde507a22ced215
johan <johan@charp.co>
2016-02-09 20:51:32 +0100

    MFCC extraction

--- main.py
+++ main.py
@@ -34,3 +34,13 @@ def read_wav_iter(dir_path=dir_path, verbose=0):
             yield data
 
 data_iter = read_wav_iter()
+
+import subprocess
+def create_MFCC(dir_path=dir_path, path_to_config='../../Programmes/openSMILE-2.1.0/config/MFCC12_0_D_A.conf', verbose=0):
+    for _, _, files in os.walk(dir_path):
+        for i, file_name in enumerate(sorted(files)):
+            if verbose:
+                print(file_name)
+            out_file_name = file_name.replace('.wav','.csv')
+            line_command = 'SMILExtract -C ' + config + ' -I ' + dir_path + file_name + ' -O ' + 'MFCC_' + out_file_name
+            subprocess.call(line_command, shell=True)
